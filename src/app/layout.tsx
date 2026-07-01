5938d7993094f34caebd9fb643d46c890731aa40
Jason Senecharles <jasons11@bu.edu>
2026-07-01 10:56:21 -0400

    Adding analytics and speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import AuthSessionProvider from "@/components/providers/session-provider";
 import Navbar from "@/components/navbar";
 import { CartProvider } from "@/context/cart-context";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({
   children,
@@ -11,6 +13,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <SpeedInsights />
+        <Analytics />
         <AuthSessionProvider>
             <Navbar />
             <CartProvider>
