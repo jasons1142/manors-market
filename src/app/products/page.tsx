96a902aba12251d906f7d8d4cce9b25ec9daaf5b
Jason Senecharles <jasons11@bu.edu>
2026-07-02 10:55:47 -0400

    Added search

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,22 +1,78 @@
 import { prisma } from "@/lib/prisma";
 import ProductCard from "@/components/products/product-card";
+import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 
-export default async function ProductsPage() {
+export default async function ProductsPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ search?: string}>;
+}) {
+
+  const { search } = await searchParams;
+
+  const query = search?.trim() || "";
+
   const products = await prisma.product.findMany({
+    where: query 
+      ? {
+        OR: [
+          {
+            name: {
+              contains: query,
+              mode: "insensitive",
+            },
+          },
+          {
+            description: {
+              contains: query,
+              mode: "insensitive",
+            },
+          },
+        ],
+      }
+    : undefined,
     orderBy: {
       createdAt: "desc",
-    },
+    } 
   });
+
+  
 
   return (
     <main className="p-6 space-y-6 bg-[#3d251e] min-h-screen">
-      <div>
-        <p className="text-white mt-2 text-xl">
+      <p className="flex text-white mt-2 text-xl justify-center items-center">
           Browse our available products.
         </p>
+      <div className="flex justify-center items-center">
+        <form className="flex w-full max-w-md overflow-hidden rounded-lg border bg-white text-black">
+          <input
+            name="search"
+            defaultValue={query}
+            placeholder="Search products..."
+            className="flex-1 p-3 outline-none"
+          />
+
+          <button className="bg-[#DCC7A6] px-5 text-[#3d251e] font-medium hover:bg-[#cbb68f]">
+            Search
+          </button>
+        </form>
       </div>
+
+      
+        {query && (
+          <p className="text-sm text-black flex justify-center items-center">
+            Showing results for "{query}"
+          </p>
+        )}
+
+        {query && (
+          <Link href="/products" className="underline flex justify-center items-center">
+            Clear search
+          </Link>
+        )}
+      
 
       {products.length === 0 ? (
         <p>No products available yet.</p>
